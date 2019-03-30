b23bc3ae6b7c4512168194bb022d672db727a79c
unknown <coolvirgo95@gmail.com>
2019-03-30 14:27:25 -0400

    added mlab functionality

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   // "URI" : "mongodb://localhost/test",
-  //   URI : "mongodb://gurpreet30:a123456@ds042138.mlab.com:42138/comp308assgn02",
-   "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b",
+    "URI" : "mongodb://gurpreet30:a123456@ds054999.mlab.com:54999/comp308asgn2_gurpreetkaur",
+   //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b",
     "secret": "SomeSecret"
 }
